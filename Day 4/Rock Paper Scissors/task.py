8e33ef81f20d7d2b7fc01a81820fce41b6840248
guysogood <potcharapol.eaksil@gmail.com>
2024-10-23 22:57:53 +0700

    Updated

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -27,48 +27,48 @@ scissors = '''
 ---.__(___)
 '''
 
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors."))
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
 list_type = [rock,paper,scissors]
 random_list = random.choice(list_type)
 
 if user_choice == 0:
-    print(rock)
-    print("Computer choose:")
+    print(list_type[0])
+    print("Computer choose: 0")
     if random_list == rock:
         print(rock)
-        print("It's draw.")
+        print("It's draw!")
     elif random_list == paper:
         print(paper)
-        print("You lose.")
+        print("You lose!")
     elif random_list == scissors:
         print(scissors)
-        print("You win.")
+        print("You win!")
 
 elif user_choice == 1:
-    print(paper)
-    print("Computer choose:")
+    print(list_type[1])
+    print("Computer choose: 1")
     if random_list == rock:
         print(rock)
-        print("You win.")
+        print("You win!")
     elif random_list == paper:
         print(paper)
-        print("It's draw.")
+        print("It's draw!")
     elif random_list == scissors:
         print(scissors)
-        print("You lose.")
+        print("You lose!")
 
 elif user_choice == 2:
-    print(scissors)
-    print("Computer choose:")
+    print(list_type[2])
+    print("Computer choose: 2")
     if random_list == rock:
         print(rock)
-        print("You lose.")
+        print("You lose!")
     elif random_list == paper:
         print(paper)
-        print("You win.")
+        print("You win!")
     elif random_list == scissors:
         print(scissors)
-        print("It's draw.")
+        print("It's draw!")
 
 else:
-    print("Invalid number")
+    print("Invalid number. You lose!")
